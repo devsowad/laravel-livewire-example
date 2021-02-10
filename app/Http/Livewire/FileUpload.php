e6cec09338b79bfb783821dd4cd0cca7b9cd1981
Sowad Mim <mdsowad2004@gmail.com>
2021-02-10 08:29:47 +0600

    Instailling alpine js package

--- app/Http/Livewire/FileUpload.php
+++ app/Http/Livewire/FileUpload.php
@@ -3,10 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class FileUpload extends Component
 {
     public $photo;
+
+    use WithFileUploads;
 
     public function render()
     {
