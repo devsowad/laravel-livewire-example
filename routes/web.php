ea1ea8f28d8f494d93f12e78ee947af09af1529d
Sowad Mim <mdsowad2004@gmail.com>
2021-03-19 07:08:29 +0600

    Adding sortable functionality.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire;
 
 
+Route::get('/image', Livewire\FileUpload::class)->name('image');
 Route::middleware('auth')->group(function () {
     Route::get('/', Livewire\Home::class)->name('home');
-    Route::get('/image', Livewire\FileUpload::class)->name('image');
     Route::get('/logout', [Livewire\Logout::class, 'logout'])->name('logout');
     Route::get('/things', Livewire\Things::class)->name('things');
 });
